2ebcbba4d1e4fe04c920c32a680e74451334ba22
jackpunt <jpeckj@gmail.com>
2024-11-06 14:18:08 -0800

    from [standalone] Hexmarket: @thegraid/*, StageComponent, AppComponent, KeyBinder, 4209, linkName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { KeyBinder } from '@thegraid/easeljs-lib';
+import type { Event } from '@thegraid/easeljs-module';
+import { StageComponent } from './stage/stage.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [StageComponent]
 })
 export class AppComponent {
-  title = 'acquire';
+  get title() { return this.titleService.getTitle(); }
+  timestamp = `${new Date().toLocaleTimeString('en-US')}`;
+  linkName: string;
+
+  constructor(private keyBinder: KeyBinder, private titleService: Title) {
+    this.linkName = `${this.title} - User Guide`;
+  }
+
+  // app.component has access to the 'Host', so we use @HostListener here
+  // Listen to all Host events and forward them to our internal EventDispatcher
+  @HostListener('document:keyup', ['$event'])
+  @HostListener('document:keydown', ['$event'])
+  @HostListener('mouseenter', ['$event'])
+  @HostListener('mouseleave', ['$event'])
+  @HostListener('focus', ['$event'])
+  @HostListener('blur', ['$event'])
+  dispatchAnEvent(event: Event) {
+    //console.log("dispatch: "+event.type);
+    this.keyBinder.dispatchEvent(event);
+  }
 }
---
+++ src/app/stage/stage.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { stime } from '@thegraid/common-lib';
+import { } from 'wicg-file-system-access';
+import { Title } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
+// import { GameSetup } from '../game-setup';
+
+@Component({
+    selector: 'stage-comp',
+    templateUrl: './stage.component.html',
+    styleUrls: ['./stage.component.css'],
+    standalone: true
+})
+export class StageComponent implements OnInit {
+
+  static idnum: number = 0;
+  getId(): string {
+    return `T${(StageComponent.idnum = StageComponent.idnum + 1)}`;
+  };
+
+  /** the query string: ?a=...&b=...&c=... =>{a: ..., b: ..., c:...} */
+  @Input('params')
+  qParams: Params = {};
+
+  @Input('width')
+  width = 1600.0;   // [pixels] size of "Viewport" of the canvas / Stage
+  @Input('height')
+  height = 800.0;   // [pixels] size of "Viewport" of the canvas / Stage
+
+  /** HTML make a \<canvas/> with this ID: */
+  mapCanvasId = `mapCanvas${this.getId()}`; // argument to new Stage(this.canvasId)
+
+  constructor(private activatedRoute: ActivatedRoute, private titleService: Title, private app: AppComponent) { }
+  ngOnInit() {
+    console.log(stime(this, ".noOnInit---"))
+    this.activatedRoute.params.subscribe(params => {
+      console.log(stime(this, ".ngOnInit: params="), params)
+    })
+    this.activatedRoute.queryParams.subscribe(params => {
+      console.log(stime(this, ".ngOnInit: queryParams="), params);
+      this.qParams = params;
+    });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(()=>this.ngAfterViewInit2(), 250) // https://bugs.chromium.org/p/chromium/issues/detail?id=1229541
+  }
+  ngAfterViewInit2() {
+    let href: string = document.location.href;
+    console.log(stime(this, ".ngAfterViewInit---"), href, "qParams=", this.qParams)
+    // const gs = new GameSetup(this.mapCanvasId, this.qParams);    // load images; new GamePlay(qParams);
+    // this.titleService.setTitle(`${this.app.title} ${gs.pageLabel}`)
+  }
+
+  // see: stream-writer.setButton
+  /** Set the HTML (fsOpenFileButton) button to do a wicg-file-system-access action */
+  static enableOpenFilePicker(method: 'showOpenFilePicker' | 'showSaveFilePicker' | 'showDirectoryPicker',
+    options: OpenFilePickerOptions & { multiple?: boolean } & SaveFilePickerOptions & DirectoryPickerOptions,
+    cb: (fileHandleAry: any) => void) {
+    const picker = window[method]       // showSaveFilePicker showDirectoryPicker
+    const fsOpenButton = document.getElementById("fsOpenFileButton")
+    if (fsOpenButton) fsOpenButton.onclick = async () => {
+      picker(options).then((value: any) => cb(value), (rej: any) => {
+        console.warn(`showOpenFilePicker failed: `, rej)
+      });
+    }
+  }
+}
